12eb96ee97ee49decbf97862e2fa252792270528
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-05-28 14:23:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -182,11 +182,11 @@ app.post('/message', (req, res) => {
                 if(bandera_opt)
                 {
                   console.log(options);
-                  /*request(options, function (error, response)
+                  request(options, function (error, response)
                   { 
                     if (error) throw new Error(error);
                     console.log(response.body);
-                  });*/
+                  });
                 }
               }
               else{result = msj_dafault; localStorage.removeItem("msj_"+conversationID);}
