75b8afbd5293758f3d72a78313ce91802abf1ab8
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-09-02 02:01:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 //var cola_opc1 = "WhatsappTest";
 var pais = "CR", nomApp = "MenuBot_CR";
 
-var OPEN_HOUR = 7;
+/*var OPEN_HOUR = 7;
 var OPEN_MINUTE = 0;
 
-//var CLOSE_HOUR = 22;
-//var CLOSE_MINUTE = 0;
+var CLOSE_HOUR = 22;
+var CLOSE_MINUTE = 0;*/
+
+var OPEN_HOUR = 0;
+var OPEN_MINUTE = 0;
 
 var CLOSE_HOUR = 23;
 var CLOSE_MINUTE = 59;
