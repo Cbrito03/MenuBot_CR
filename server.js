407861e1714ee33fcc58e537e4a86c06336b9679
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-09-25 23:29:46 -0500

    dds
    sd

--- server.js
+++ server.js
@@ -490,7 +490,149 @@ app.post('/message', async (req, res) => {
                       "RUT":"1-9"
                     }
                   }
-                }             
+                }
+                else
+                {
+                  console.log("Entro a "+ channel.toLowerCase() +" - whatsapp");
+
+                   var horarios = horario.validarHorario_WA();
+                  
+                  for(var i = 0; i < cadena.length; i++)
+                  {
+                    for(var atr in msj_wa.palabras)
+                    {                 
+                      if(cadena[i] === "pagar" || cadena[i] === "factura" ){ localStorage.removeItem("msj_"+conversationID); }
+
+                      if(atr.toLowerCase() === cadena[i])
+                      {
+                        opcion = cadena[i];
+                        msj_buscar = cadena[i];
+                        result_action = msj_wa.palabras[atr].action;
+                        result_messages = msj_wa.palabras[atr].messages;                    
+                        bandera = true;
+                        bandera_opt = true;
+                        break;
+                      }
+                    }
+
+                    if(bandera){ break; }
+                  }
+
+                  console.log("[Brito] :: [message] :: [msj_buscar_opcion] :: " + msj_buscar_opcion);
+
+                  if(localStorage.getItem("msj_"+conversationID) == null) // No existe localStorage
+                  {
+                    if(msj_buscar == "factura" || msj_buscar == "pagar" )
+                    {
+                      localStorage.setItem("msj_"+conversationID, msj_buscar);
+                      console.log('[Brito] :: [message] :: [Se crea LocalStrogae para '+msj_buscar+'] :: ', localStorage.getItem("msj_"+conversationID));
+                    }
+                    else if(!bandera)
+                    {
+                      result_messages = msj_wa.msj_default.messages;
+                      result_action = msj_wa.msj_default.action;
+                    }  
+                  }
+                  else // esite localStorage
+                  {                    
+                    console.log('[Brito] :: [message] :: [Existe Storage] :: ' + localStorage.getItem("msj_"+conversationID));
+                    
+                    
+                    var msj_storage = localStorage.getItem("msj_"+conversationID);
+
+                    console.log('[Brito] :: [message] :: [msj_storage] :: ' + msj_storage + ' :: [msj_buscar_opcion] :: ' + msj_buscar_opcion);
+
+                    if((msj_storage == "factura" || msj_storage == "pagar") && msj_buscar_opcion == "asesor")
+                    {
+                      opcion = msj_storage + " - asesor";
+
+                      localStorage.removeItem("msj_"+conversationID);
+
+                      if(config.horario_24_7 == true || horarios)
+                      {
+                        if(msj_storage == "factura")
+                        {
+                          result_messages = msj_wa.msj_factura_asesor.messages;
+                          result_action = msj_wa.msj_factura_asesor.action;
+                        } 
+                        else if(msj_storage == "pagar")
+                        {
+                          result_messages = msj_wa.msj_pagar_asesor.messages;
+                          result_action = msj_wa.msj_pagar_asesor.action;
+                        }
+
+                        bandera_tranferido = true;  
+                      }
+                      else
+                      {
+                        console.log("[Brito] :: [No cumple horario habil para Configuración en Asesor] :: [horarios] :: "+horarios);
+                        localStorage.removeItem("msj_"+conversationID+"_horario");
+                        localStorage.setItem("msj_"+conversationID+"_horario", "fueraHorario");
+                        result_messages = msj_wa.msj_fuera_horario.messages;
+                        result_action = msj_wa.msj_fuera_horario.action;                                      
+                        bandera_fueraHorario = true;                                
+                      }
+
+                      bandera_opt = true;
+                      bandera = true;               
+                    }
+                    else
+                    {
+                      localStorage.removeItem("msj_"+conversationID);
+
+                      if(!bandera)
+                      {
+                        result_messages = msj_wa.msj_default.messages;
+                        result_action = msj_wa.msj_default.action;
+                      }
+                    }
+                  }
+
+                  var options = {
+                    method : 'post',
+                    url : config.url_estd,
+                    headers : { 'Content-Type': 'application/json'},
+                    data: JSON.stringify({
+                      "conversacion_id" : conversationID,
+                      "pais" : config.info.pais,
+                      "app" : config.info.nomApp,
+                      "opcion" : opcion,
+                      "rrss" : "WA",
+                      "transferencia" : bandera_tranferido,
+                      "fueraHorario" : bandera_fueraHorario,
+                      "grupoACD" : result_action.queue        
+                    })
+                  };          
+
+                  if(bandera == true)
+                  {
+                    if(bandera_opt)
+                    {
+                      console.log(options);
+                      var resultado_axios = await axios(options);
+                      console.log("[Resultado AXIOS] :: ");
+                      console.log(resultado_axios);
+                    }                 
+                  }
+                  else
+                  {
+                    localStorage.removeItem("msj_"+conversationID);
+                    result_messages = msj_wa.msj_default.messages;
+                    result_action = msj_wa.msj_default.action;
+                  }
+
+                  console.log("[Brito] :: [channel] :: ", channel, " :: [opcion] :: ", opcion);
+                                  
+                  resultado = {
+                    "context": context,
+                    "action": result_action,
+                    "messages": result_messages,
+                    "additionalInfo": {
+                      "key":"RUT",
+                      "RUT":"1-9"
+                    }
+                  }
+                }          
               }
               else if(context.lastInteractionFinishType == "CLIENT_TIMEOUT")
               {
